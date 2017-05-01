1a9bfc6a6c883a7cc20019154621773ef85847fe
John Goerzen <jgoerzen@complete.org>
2017-05-01 03:19:52 +0000

    sed fixes

--- dos-bbs/setup/setup.sh
+++ dos-bbs/setup/setup.sh
@@ -29,10 +29,10 @@ echo "cd \\adf" >> /dos/dosbox.conf
 echo "adf.exe COM1 3F8 4 115200 8192 8192 8" >> /dos/dosbox.conf
 
 # We don't need to waste CPU generating every frame.
-sed -i 's/frameskip=0/frameskip=100' /dos/dosbox.conf
+sed -i 's/frameskip=0/frameskip=100/' /dos/dosbox.conf
 
 # Disable sound.
-sed -i 's/nosound=false/nosound=true' /dos/dosbox.conf
+sed -i 's/nosound=false/nosound=true/' /dos/dosbox.conf
 
 # Generate the telnetbbs base.
 sed 's/serial1=dummy/serial1=modem listenport:__LISTEN_PORT__/' \
