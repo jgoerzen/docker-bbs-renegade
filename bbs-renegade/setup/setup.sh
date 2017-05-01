9a1c6ccf260e8bca834387990ca1bde0f17da60c
John Goerzen <jgoerzen@complete.org>
2017-05-01 03:55:37 +0000

    update

--- bbs-renegade/setup/setup.sh
+++ bbs-renegade/setup/setup.sh
@@ -6,9 +6,10 @@ set -x
 cd /dos/drive_c
 unzip /tmp/setup/unofficial.rgbbsfullv120a.zip
 
-sed -i -e 's/port = 3023/port = 23/' \
-    -e 's/base_port = 3024/base_port = 7001/' \
-    -e 's/display = :0.0/display = :1/' \
-    -e 's,dosboxt = dosbox.conf.template,dosboxt = /dos/dosbox-telnetbbs-template.conf,'  \
-    /dos/TelnetBBS-master/telnetbbs.conf
+cd /dos
+DBCONF="/dos/dosbox-telnetbbs-template.conf"
+echo "C:" >>$DBCONF
+echo "cd rg" >>$DBCONF
+echo "renegade.exe /N__NODE__ -Q -B115200" >>$DBCONF
+echo "exit" >>$DBCONF
 
