9a8796a0f2eec58d0996fcc42d07c500be7af649
John Goerzen <jgoerzen@complete.org>
2017-05-01 00:25:32 -0500

    Add scripts area

--- dosbox/setup.sh
+++ dosbox/setup.sh
@@ -5,13 +5,14 @@ set -x
 
 mkdir -p /dos/drive_{c,d,e,f,g,y}
 cp -r /usr/lib/dosemu/drive_z/bin /dos/drive_y/DOS
+mkdir /dos/drive_y/SCRIPTS
 
 DOSBOXCONF="`dosbox -printconf`"
 mv "$DOSBOXCONF" /dos/dosbox.conf
 for ASDF in c d e f g h y; do 
   echo "mount $ASDF /dos/drive_$ASDF" >> /dos/dosbox.conf
 done
-echo 'path %PATH%;Y:\DOS' >> /dos/dosbox.conf
+echo 'path %PATH%;Y:\DOS;Y:\SCRIPTS' >> /dos/dosbox.conf
 
 # Fix an issue with the VNC console
 sed -i 's/usescancodes=true/usescancodes=false/' /dos/dosbox.conf
