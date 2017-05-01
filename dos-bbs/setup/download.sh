85ea4a1dcb4a7c60848c0ce2164dfd6a08092590
John Goerzen <jgoerzen@complete.org>
2017-05-01 02:38:16 +0000

    Adding some downloads

--- dos-bbs/setup/download.sh
+++ dos-bbs/setup/download.sh
@@ -8,3 +8,11 @@ wget https://ftp.sunet.se/mirror/archive/ftp.sunet.se/pub/simtelnet/msdos/fossil
 wget https://github.com/Geryon/TelnetBBS/archive/master.zip
 
 sha256sum -c < sums
+
+mkdir doors
+cd doors
+wget http://archives.scovetta.com/pub/fehq/BBSDoorGames/2002v309.zip
+wget http://www.slbbs.com/files/doorgame/lord407.zip
+wget http://www.slbbs.com/files/doorgame/lord407-patch.zip
+sha256sum -c < ../doorsums
+
--- dos-bbs/setup/setup.sh
+++ dos-bbs/setup/setup.sh
@@ -11,6 +11,19 @@ mkdir adf
 cd adf
 unzip /tmp/setup/adf_150.zip
 
+cd /dos/drive_d
+mkdir doors
+cd doors
+mkdir tw2002
+cd tw2002
+unzip /tmp/setup/doors/2002v309.zip
+cd ..
+mkdir lord
+cd lord
+unzip -L /tmp/setup/doors/lord407.zip
+unzip -L lord.zip
+unzip -o -L /tmp/setup/doors/lord407-patch.zip
+
 echo "d:" >> /dos/dosbox.conf
 echo "cd \\adf" >> /dos/dosbox.conf
 echo "adf.exe COM1 3F8 4 115200 8192 8192 8" >> /dos/dosbox.conf
