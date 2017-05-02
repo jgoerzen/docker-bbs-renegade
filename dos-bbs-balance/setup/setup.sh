bf52c7c10b14a2432e32cfbcd578d830358812eb
John Goerzen <jgoerzen@complete.org>
2017-05-01 22:33:26 -0500

    Build corrections for dos-bbs-balance

--- dos-bbs-balance/setup/setup.sh
+++ dos-bbs-balance/setup/setup.sh
@@ -8,9 +8,6 @@ set -x
 #    -e 's/frameskip=0/frameskip=30/' \
 #  < /dos/dosbox.conf > /dos/dosbox-telnetbbs-template.conf
 
-# Set up the DOSBox modem.
-sed -i 's/serial1=dummy/serial1=modem listenport:5000/' /dos/dosbox.conf
-
 # We don't want to run this.
 rm /etc/supervisor/conf.d/telnetbbs.conf
 
